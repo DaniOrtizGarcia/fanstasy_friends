4763bc8fa1e9a5e0ce815635d308fabc8f443bed
Daniel Ortiz <daniortizgar@gmail.com>
2025-10-08 20:39:08 +0200

    Add table-texts utility for responsive header texts in mobile and desktop views

---
+++ src/utils/table-texts.ts
@@ -0,0 +1,20 @@
+export const headerTexts = {
+  mobile: {
+    name: 'Nombre',
+    wins: 'V',
+    losses: 'D',
+    goals: 'G',
+    points: 'Pts'
+  },
+  desktop: {
+    name: 'Jugador',
+    wins: 'Victorias',
+    losses: 'Derrotas',
+    goals: 'Goles',
+    points: 'Puntos'
+  }
+};
+
+export const getResponsiveTexts = (isMobile: boolean) => {
+  return isMobile ? headerTexts.mobile : headerTexts.desktop;
+};

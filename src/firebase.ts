9f4b4d143c3ee2549f34d9e5855b53d8123a3054
Daniel Ortiz <daniortizgar@gmail.com>
2025-10-25 13:36:43 +0200

    Add .env.example file for Firebase configuration and update .gitignore to exclude environment variable files

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,14 +8,14 @@ import { getFirestore, collection, writeBatch, doc, getDocs, deleteDoc } from "f
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9GrWVPt6ktlEc3pW7GT-qGaLhtubPx_M",
-  authDomain: "cojos-fantasy.firebaseapp.com",
-  databaseURL: "https://cojos-fantasy-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "cojos-fantasy",
-  storageBucket: "cojos-fantasy.firebasestorage.app",
-  messagingSenderId: "70043218957",
-  appId: "1:70043218957:web:0db6f8c2db4bc84498d144",
-  measurementId: "G-73B6SQTL6J"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
